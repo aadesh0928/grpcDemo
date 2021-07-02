4ff097a1c09b3f5fa4bbb908395d1efc78ecd769
aadesh0928 <46253413+aadesh0928@users.noreply.github.com>
2021-07-02 12:43:24 +0530

    Add files via upload

---
+++ GrpcAPIServer/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcAPIServer.Models
+{
+    public class Employee
+    {
+    }
+}
